8158f661b25b241048309d1c3ce849684406c7df
menacingsoul <adarsh14103@gmail.com>
2023-09-19 16:46:49 +0530

    .

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,7 +1,7 @@
 import { prisma } from "@/utils/db";
 import { getUserClerkID } from "@/utils/auth";
 const getEntries = async () => {
-    const user = await getUserClerkID()
+    const user = await getUserClerkID
     const entries = await prisma.journalEntry
     
 }
